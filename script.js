ca1b616f2306dd9f3efe16aa95afc95b9558b6bb
zaaraa-z <zamanian.zahra@gmail.com>
2021-12-19 13:15:04 +0330

    gallery interactions coding

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   //animation on scroll
   $(window).scroll(function () {
     let scrollPosition = $(this).scrollTop();
-    // console.log(scrollPosition);
     if (scrollPosition >= 900) {
       $('.who-img').addClass('onscroll-from-left-anim');
       $('.who-text').addClass('onscroll-from-right-anim');
@@ -10,5 +9,21 @@ $(document).ready(function () {
       $('.who-img').removeClass('onscroll-from-left-anim');
       $('.who-text').removeClass('onscroll-from-right-anim');
     }
+  });
+
+  //gallery control
+  $('.gallery-li').click(function () {
+    const clickedAttr = $(this).attr('data-filter');
+    if (clickedAttr == 'all') {
+      $('.all-imgs').show(300);
+    } else {
+      $('.all-imgs')
+        .not('.' + clickedAttr)
+        .hide(300);
+      $('.all-imgs')
+        .filter('.' + clickedAttr)
+        .show(300);
+    }
+    $(this).addClass('active').siblings().removeClass('active');
   });
 });
