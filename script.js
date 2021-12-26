40c761d82af58dd3d71064d392cf68f154d97c52
zaaraa-z <zamanian.zahra@gmail.com>
2021-12-26 20:45:10 +0330

    add animations to pricing cards

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  //animation on scroll
+  //animation on scroll: our mission
   $(window).scroll(function () {
     let scrollPosition = $(this).scrollTop();
     if (scrollPosition >= 900) {
@@ -8,6 +8,16 @@ $(document).ready(function () {
     } else {
       $('.who-img').removeClass('onscroll-from-left-anim');
       $('.who-text').removeClass('onscroll-from-right-anim');
+    }
+
+    if (scrollPosition >= 4100) {
+      $('.pricing-card-1').addClass('card-to-right');
+      $('.pricing-card-3').addClass('card-to-left');
+      $('.pricing-card-2').addClass('card-to-top');
+    } else {
+      $('.pricing-card-1').removeClass('card-to-right');
+      $('.pricing-card-3').removeClass('card-to-left');
+      $('.pricing-card-2').removeClass('card-to-top');
     }
   });
 
